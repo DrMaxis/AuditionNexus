c56e63ae7ee42faddb7f6107954ae258f4aafd71
DrMaxis <Grandmasterantwii@gmail.com>
2020-01-13 23:15:53 -0500

    Update game.blade.php

--- resources/views/frontend/user/account/tabs/game.blade.php
+++ resources/views/frontend/user/account/tabs/game.blade.php
@@ -4,7 +4,6 @@
     @if($logged_in_user->otp_expiration_date->isPast() )
 
     <div class="col-md-6">
-        <p> Your currecnt OTP is {{$logged_in_user->otp_code}} </p>
         <p>
             Your OTP has expired and you will need to generate a new one.
 
@@ -14,16 +13,21 @@
             <input id="gen0" data-userID="{{$logged_in_user->id}}" type="button" value="Generate One time Password!" />
         </form>
     </div>
-@else 
+    @else
     <div class="col-md-6">
-        <p> Your currecnt OTP is {{$logged_in_user->otp_code}} </p>
+            <p id="otp_code"> Your currecnt OTP is:
+            <span>
+                <button type="button" id="otp_reveal_btn" class="btn btn-primary" data-clipboard-text="{{$logged_in_user->otp_code}}">Reveal & Copy</button>
+            </span>
+        </p>
+        <input type="hidden" value="{{$logged_in_user->otp_code}}" id="otp_hidden">
         <p>
             Your OTP will expire at {{ timezone()->convertToLocal($logged_in_user->otp_expiration_date) }}
             ({{ $logged_in_user->otp_expiration_date->diffForHumans() }})
 
         </p>
     </div>
-@endif
+    @endif
 
     @else
 
@@ -36,7 +40,12 @@
 
 
 @section('page-scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.4/clipboard.min.js"></script>
+<script>
 
+new ClipboardJS('#otp_reveal_btn');
+
+</script>
 <script>
     (function() {
     
@@ -62,5 +71,20 @@
     });
     })();
     
+</script>
+
+
+<script>
+    var otp = "{{$logged_in_user->otp_code}}";
+$('#otp_reveal_btn').on('click', function() {
+    
+    if($('#otp_reveal_btn').text() != 'Reveal & Copy') {
+        $('#otp_reveal_btn').text('Reveal & Copy');
+    } else {
+        $('#otp_reveal_btn').text(otp);        
+    }
+})
+
+
 </script>
 @endsection
